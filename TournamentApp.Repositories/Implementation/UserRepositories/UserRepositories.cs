fbc13f654385d5211d255808dbf7fbed647024b9
fabiancogghe <cogghefabian@gmai.com>
2021-05-13 13:30:51 +0200

    Add user Repositories with test data

---
+++ TournamentApp.Repositories/Implementation/UserRepositories/UserRepositories.cs
@@ -0,0 +1,7 @@
+namespace TournamentApp.Repositories.Implementation.UserRepositories
+{
+    public class UserRepositories
+    {
+        
+    }
+}
---
+++ TournamentApp.Repositories/Interfaces/UserRepositories/IUserRepositories.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using TournamentApp.Model;
+
+namespace TournamentApp.Repositories.Interfaces.UserRepositories
+{
+    public interface IUserRepositories: ICrudRepository<User>
+    {
+        public static User Testing(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(
+                new User {
+                    Id = 1,
+                    Player = new Player
+                    {
+                        Id = 1,
+                        Name = "goku"
+                    },
+                Password = "goku",
+                Role = "manager" }
+            );
+
+            users.Add(
+                new User {
+                    Id = 2,
+                    Player = new Player
+                    {
+                        Id = 2,
+                        Name = "goku"
+                    },
+                    Password = "goku",
+                    Role = "manager" }
+            );
+
+            users.Add(
+                new User {
+                    Id = 3,
+                    Player = new Player
+                    {
+                        Id = 3,
+                        Name = "goku"
+                    },
+                    Password = "goku",
+                    Role = "manager" }
+            );
+            return users.FirstOrDefault(x => x.Player.Name.ToLower() == username.ToLower());
+        }
+    }
+}
