0772a6f8b744f513c1617e697bc372169bc7587b
fabiancogghe <cogghefabian@gmai.com>
2021-06-09 16:18:33 +0200

    Add validation based on Attributes

--- TournamentApp.Api/Controllers/UserController.cs
+++ TournamentApp.Api/Controllers/UserController.cs
@@ -24,7 +24,7 @@ namespace TournamentApp.Api.Controllers
         {
             if (!ModelState.IsValid) return BadRequest(ModelState); //Change this to attribute, or middellware
 
-            if (_userService.CheckIfEmailIsAlreadyRegistered(userRegisterDto.Email)){ return BadRequest("Somebody with that email already exists"); }
+            if (!_userService.CheckIfEmailIsAlreadyRegistered(userRegisterDto.Email)){ return BadRequest("Somebody with that email already exists"); }
             var createdUser = _userService.Register(userRegisterDto);
             return Created(nameof(UserInfo), createdUser);
         }
