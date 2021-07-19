b13ccd6e7b8eb1f203ea3e9db9e0a60288939d82
Cogghe_Fabian <CoggheFabian@hotmail.com>
2021-07-19 14:08:24 +0200

    Add MockUserTournamentRepo.cs

---
+++ TournamentApp.Repositories/Implementation/UserTournamentRepo/MockUserTournamentRepo.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using TournamentApp.Repositories.Interfaces;
+using TournamentApp.Shared.Dtos;
+
+namespace TournamentApp.Repositories.Implementation.UserTournamentRepo
+{
+    public class MockUserTournamentRepo : IUserTournamentRepository
+    {
+        public IQueryable<TournamentWithUserDto> GetAUserWithHisTournaments(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
